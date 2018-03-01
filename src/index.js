8293a6c1b4dfe26231ef558aefe64c79ca67375a
isushkevich <ivan.sushik@gmail.com>
2018-03-01 17:38:28 +0300

    better algorithm

--- src/index.js
+++ src/index.js
@@ -1,15 +1,9 @@
 module.exports = function getZerosCount( number ) {
-  let count = 0;
-  count = Math.floor( number / 5 );
-  count += Math.floor( number / 25 );
-  count += Math.floor( number / 125 );
-  count += Math.floor( number / 625 );
-  count += Math.floor( number / 3125 );
-  count += Math.floor( number / 15625 );
-  count += Math.floor( number / 78125 );
-  count += Math.floor( number / 390625 );
-  count += Math.floor( number / 1953125 );
-  count += Math.floor( number / 9765625 );
-  count += Math.floor( number / 48828125 );
+  let count = 0,
+    factor = 5;
+  while ( factor <= number ) {
+    count += Math.floor( number / factor );
+    factor = factor * 5;
+  }
   return count;
 }
