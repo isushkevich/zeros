bb2dfc6403fd93de5aa2d4b5a07a3069bd376296
isushkevich <ivan.sushik@gmail.com>
2018-03-01 17:47:00 +0300

    grammar fix

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 module.exports = function getZerosCount( number ) {
   let count = 0,
-    factor = 5;
-  while ( factor <= number ) {
-    count += Math.floor( number / factor );
-    factor = factor * 5;
+    divisor = 5;
+  while ( divisor <= number ) {
+    count += Math.floor( number / divisor );
+    divisor = divisor * 5;
   }
   return count;
 }
