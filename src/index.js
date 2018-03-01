7f9a631096659be4c92423e06689ebc688eec0a2
isushkevich <ivan.sushik@gmail.com>
2018-03-01 03:29:20 +0300

    working solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
-module.exports = function getZerosCount(number) {
-  // your implementation
+module.exports = function getZerosCount( number ) {
+  let count = 0;
+  count = Math.floor( number / 5 );
+  count += Math.floor( number / 25 );
+  count += Math.floor( number / 125 );
+  count += Math.floor( number / 625 );
+  count += Math.floor( number / 3125 );
+  count += Math.floor( number / 15625 );
+  count += Math.floor( number / 78125 );
+  count += Math.floor( number / 390625 );
+  count += Math.floor( number / 1953125 );
+  count += Math.floor( number / 9765625 );
+  count += Math.floor( number / 48828125 );
+  return count;
 }
